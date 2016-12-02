efc3c860bb9fe9d12b8149ef7761c6a702085806
tzduan <tzduan@Ctrip.com>
2016-12-02 16:37:55 +0800

    first commit

---
+++ test/one.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /Users/tzduan/WorkSpace/dev/android_2
---
+++ test/svn_update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+source ~/Other/shell/config
+
+int=0
+while (($int<${#SVN_PATH[@]}))
+do
+  svn update ${SVN_PATH[int]}
+  let "int++"
+done
